039dd8a5d46c829a79bec7d5fb65d222f65ddccf
miramila <bonny.su@outlook.com>
2024-04-24 22:02:16 -0400

    add question into text

--- bert_training.py
+++ bert_training.py
@@ -66,9 +66,9 @@ print_trainable_parameters(model)
 training_args = TrainingArguments(
     output_dir='./results_bert',
     overwrite_output_dir=True,
-    learning_rate=2e-5,
-    per_device_train_batch_size=64,
-    per_device_eval_batch_size=64,
+    learning_rate=1e-5,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
     do_eval=True,
     seed = 12345,
     evaluation_strategy='steps',
